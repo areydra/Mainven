4a05cf26a6b1a6b59eb95a4cadf7adf9b3c3b709
Areydra Desfikriandre <areydra@gmail.com>
2025-07-19 08:38:12 +0700

    Initial Commit

---
+++ Mainven/MainvenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainvenApp.swift
+//  Mainven
+//
+//  Created by Areydra Desfikriandre on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MainvenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
