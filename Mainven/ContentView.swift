47358514e2bc0ba97819881bb30e22b403ff9a7c
Areydra Desfikriandre <areydra@gmail.com>
2025-07-19 10:17:15 +0700

    Initialize MVP

--- Mainven/ContentView.swift
+++ Mainven/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeTab()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            ProductsTab()
+                .tabItem {
+                    Image(systemName: "list.bullet.rectangle.fill")
+                    Text("Products")
+                }
+            ContactsTab()
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("Contacts")
+                }
+            TransactionsTab()
+                .tabItem {
+                    Image(systemName: "dollarsign.square.fill")
+                    Text("Transactions")
+                }
         }
-        .padding()
     }
 }
 
